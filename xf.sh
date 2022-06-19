41723ada360787d89bdad27de0034db479ecbd61
Tianxi田溪 <96676423+tianxily@users.noreply.github.com>
2022-06-19 14:00:58 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "3f88a1aa-20bd-46eb-8115-4e0084b9b8ec",
                         "alterId": 0
                     }
                 ],
